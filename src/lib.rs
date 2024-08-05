e1b240dc80685f60cb5388cb67c8263995ddc1a1
Shashi <vshashi01@gmail.com>
2024-08-05 22:09:06 +0200

    Added xml fields and attributes to the tree representation

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 // mod threemf_reader;
 mod threemf;
+use quick_xml::events::attributes;
 use threemf::threemf_reader;
+use xml_dom::level2::{ext::Namespaced, CharacterData, Document, Element, Node, NodeType, RefNode};
 
-use std::{ffi::OsStr, fs, io::BufReader, path::PathBuf};
+use std::{ffi::OsStr, fmt::format, fs, path::PathBuf};
 
-use egui::{DroppedFile, FontId, RichText};
-// use threemf_reader::load_threemf_get_root_model_file_as_string;
+use anyhow::Result;
+use egui::{ahash::HashMap, CollapsingHeader, DroppedFile, FontId, RichText};
 
 pub struct MyApp {
     name: String,
     dropped_files: Vec<DroppedFile>,
-    file_to_render: Option<DroppedFile>,
+    file_to_render: Option<String>,
     font_size: f32,
+    trees: Option<Vec<Tree>>,
 }
 
 impl Default for MyApp {
@@ -21,21 +24,30 @@ impl Default for MyApp {
             dropped_files: Vec::new(),
             file_to_render: None,
             font_size: 14.0,
+            trees: None,
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if let Some(trees) = &self.trees {
+            egui::SidePanel::left("left_panel")
+                .resizable(true)
+                .default_width(100.0)
+                .show(ctx, |ui| {
+                    egui::ScrollArea::both().auto_shrink(false).show(ui, |ui| {
+                        let mut count = 1;
+                        for tree in trees {
+                            tree.ui_impl(ui, 0, &format!("{} - {}", tree.name, count));
+                            count += 1;
+                        }
+                    });
+                });
+        }
         egui::CentralPanel::default().show(ctx, |ui| {
-            if let Some(file) = &self.file_to_render {
-                let path = file.path.as_ref().unwrap();
-                let text_to_display = process_file_and_get_text(&path);
-                // let text_string = fs::read_to_string(path).unwrap();
-                // ui.label(format!(
-                //     "{:?} can be rendered now",
-                //     path.file_name().unwrap()
-                // ));
+            if let Some(string) = &self.file_to_render {
+                let text_to_display = string.clone();
                 ui.vertical(|ui| {
                     ui.horizontal_top(|ui| {
                         if ui.button("+").clicked() {
@@ -46,6 +58,7 @@ impl eframe::App for MyApp {
                         }
                         if ui.button("Clear content").clicked() {
                             self.file_to_render = None;
+                            self.trees = None;
                         }
                     });
 
@@ -78,7 +91,20 @@ impl eframe::App for MyApp {
                     let file = &self.dropped_files[i];
                     if let Some(path) = &file.path {
                         if can_process_file(path.to_path_buf()) {
-                            self.file_to_render = Some(file.clone());
+                            self.file_to_render = Some(process_file_and_get_text(path));
+                            if process_file_if_extension_matches(path, "3mf")
+                                && self.file_to_render.is_some()
+                            {
+                                let result = generate_tree_from_3mf_xml_dom(
+                                    &self.file_to_render.as_ref().unwrap(),
+                                );
+
+                                match result {
+                                    Ok(trees) => self.trees = Some(trees),
+                                    Err(e) => println!("{:?}", e),
+                                }
+                            }
+
                             break;
                         }
                     }
@@ -99,7 +125,6 @@ impl eframe::App for MyApp {
 fn process_file_and_get_text(path: &PathBuf) -> String {
     let text_string = match path.extension().and_then(OsStr::to_str) {
         Some("3mf") => {
-            //do nothing
             if let Ok(file) = fs::File::open(path) {
                 return threemf_reader::load_threemf_get_root_model_file_as_string(file).unwrap();
             }
@@ -111,6 +136,10 @@ fn process_file_and_get_text(path: &PathBuf) -> String {
     };
 
     text_string
+}
+
+fn process_file_if_extension_matches(path: &PathBuf, target_ext: &str) -> bool {
+    path.extension().unwrap().to_str().unwrap() == target_ext
 }
 
 // Preview hovering files:
@@ -165,4 +194,116 @@ fn can_process_file(path: PathBuf) -> bool {
     };
 
     result
+}
+
+#[derive(Debug)]
+struct Tree {
+    name: String,
+    content: Option<String>,
+    attributes: Option<Vec<(String, String)>>,
+    childs: Option<Vec<Tree>>,
+}
+
+impl Tree {
+    fn ui_impl(&self, ui: &mut egui::Ui, depth: usize, unique_id: &str) {
+        let name = if let Some(content) = &self.content {
+            format!("{} - {}", self.name, content)
+        } else {
+            self.name.clone()
+        };
+        CollapsingHeader::new(name)
+            .default_open(depth < 1)
+            .id_source(unique_id)
+            .show(ui, |ui| {
+                if let Some(attributes) = &self.attributes {
+                    for attribute in attributes {
+                        ui.label(format!("{} - {}", attribute.0, attribute.1));
+                    }
+                }
+
+                self.children_ui(ui, depth)
+            });
+    }
+
+    fn children_ui(&self, ui: &mut egui::Ui, depth: usize) {
+        if let Some(trees) = &self.childs {
+            let mut count = 0;
+            for tree in trees {
+                tree.ui_impl(ui, depth + 1, &format!("{} - {}", tree.name, count));
+                count += 1;
+            }
+        }
+    }
+}
+
+fn generate_tree_from_3mf_xml_dom(xml_content: &String) -> Result<Vec<Tree>> {
+    let dom = threemf_reader::get_xml_dom_from_3mf_model_file_string(xml_content)?;
+    println!("{:?}", dom);
+    let trees = process_dom(dom);
+    // println!("{:?}", trees);
+
+    Ok(trees.0.unwrap())
+}
+
+fn process_dom(ref_node: RefNode) -> (Option<Vec<Tree>>, Option<String>) {
+    let mut sub_trees = Vec::new();
+    let mut sub_content = String::new();
+    for node in ref_node.child_nodes() {
+        let name = node.local_name();
+        let mut attributes: Vec<(String, String)> = Vec::new();
+
+        if node.node_type() == NodeType::Element {
+            let attributes_map = node.attributes();
+
+            for entry in attributes_map {
+                if entry.1.node_type() == NodeType::Attribute {
+                    let attribute_name = entry.0.local_name().to_string();
+                    let mut attribute_value = String::new();
+
+                    if let Some(value) = entry.1.data() {
+                        attribute_value.push_str(&value);
+                    } else {
+                        for attribute_child_node in entry.1.child_nodes() {
+                            if let Some(value) = attribute_child_node.data() {
+                                attribute_value.push_str(&value);
+                                // break;
+                            }
+                        }
+                    };
+                    attributes.push((attribute_name, attribute_value));
+                }
+            }
+
+            let (childs, entry) = if node.has_child_nodes() {
+                process_dom(node)
+            } else {
+                (None, None)
+            };
+
+            sub_trees.push(Tree {
+                name,
+                content: entry,
+                attributes: Some(attributes),
+                childs,
+            });
+        } else {
+            if let Some(data) = node.data() {
+                sub_content.push_str(&data);
+            }
+        }
+    }
+
+    let trees = if sub_trees.len() >= 1 {
+        Some(sub_trees)
+    } else {
+        None
+    };
+
+    let content = if sub_content.len() >= 1 {
+        Some(sub_content)
+    } else {
+        None
+    };
+
+    (trees, content)
 }
