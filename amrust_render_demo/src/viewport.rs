74584b830d4d602b61636f274a50cfe78dfee1c0
Shashitharan Velosamy <shashitharan.velosamy@materialise.be>
2026-01-12 15:11:19 +0100

    Improved view manipulation

--- amrust_render_demo/src/viewport.rs
+++ amrust_render_demo/src/viewport.rs
@@ -23,26 +23,35 @@ impl Viewport3D {
 
         // Track scroll for zoom
         // use only the inner response to ensure mouse only responses to the viewport region
-        if ui_response.inner.ctx.input(|i| i.raw_scroll_delta.y != 0.0) {
-            let scroll = ui.ctx().input(|i| i.raw_scroll_delta.y);
-            camera_data.transform(camera::CameraTransform::Zoom(scroll * 0.0001));
-            ui.ctx().request_repaint();
+        if ui_response.inner.hovered() {
+            let scroll_delta = ui.ctx().input(|i| i.raw_scroll_delta.y);
+            if scroll_delta.abs() > 0.0 {
+                camera_data.transform(camera::CameraTransform::Zoom(scroll_delta * 0.0001));
+                ui.ctx().request_repaint();
+            }
         }
 
-        if ui_response.inner.dragged() {
+        if ui_response.inner.dragged_by(egui::PointerButton::Secondary) {
             let delta = ui_response.inner.drag_delta();
             // println!("Drag delta is: {:?}", delta);
             let drag_sensitivity = 0.01;
             camera_data.transform(camera::CameraTransform::Rotate {
                 pivot: bbox.center(),
                 rotation_axis: glam::Vec3::Y,
-                angle: delta.x * drag_sensitivity,
+                angle: -delta.x * drag_sensitivity,
             });
             camera_data.transform(camera::CameraTransform::Rotate {
                 pivot: bbox.center(),
                 rotation_axis: glam::Vec3::X,
-                angle: delta.y * drag_sensitivity,
+                angle: -delta.y * drag_sensitivity,
             });
+            ui.ctx().request_repaint();
+        } else if ui_response.inner.dragged_by(egui::PointerButton::Middle) {
+            let delta = ui_response.inner.drag_delta();
+            let pan_sensitivity = 0.01;
+            camera_data.transform(camera::CameraTransform::Pan(
+                glam::Vec3::new(-delta.x, -delta.y, 0.0) * pan_sensitivity,
+            ));
             ui.ctx().request_repaint();
         } else if ui_response.inner.clicked() {
             println!("Clicked in the region");
