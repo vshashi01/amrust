0c68911c834894537a3e0216a6c2c0065470e509
Shashitharan Velosamy <shashitharan.velosamy@materialise.be>
2026-02-10 10:37:31 +0100

    Fixed screen space buffer update

--- amrust_render/src/screen_space.rs
+++ amrust_render/src/screen_space.rs
@@ -16,13 +16,8 @@ pub struct ScreenSpace {
 
 impl ScreenSpace {
     pub fn new(device: &wgpu::Device, width: f32, height: f32) -> Self {
-        let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Screen Space buffer"),
-            contents: bytemuck::cast_slice(&[ScreenSpaceUniform {
-                screen_size: [width, height],
-            }]),
-            usage: wgpu::BufferUsages::UNIFORM,
-        });
+        let uniform = Self::create_uniform(width, height);
+        let buffer = Self::create_uniform_buffer(uniform, device);
 
         Self {
             width,
@@ -37,13 +32,22 @@ impl ScreenSpace {
     }
 
     pub fn write_buffer(&self, queue: &wgpu::Queue) {
-        queue.write_buffer(
-            &self.buffer,
-            0,
-            bytemuck::cast_slice(&[ScreenSpaceUniform {
-                screen_size: [self.width, self.height],
-            }]),
-        );
+        let uniform = Self::create_uniform(self.width, self.height);
+        queue.write_buffer(&self.buffer, 0, bytemuck::cast_slice(&[uniform]));
+    }
+
+    fn create_uniform(width: f32, height: f32) -> ScreenSpaceUniform {
+        ScreenSpaceUniform {
+            screen_size: [width, height],
+        }
+    }
+
+    fn create_uniform_buffer(uniform: ScreenSpaceUniform, device: &wgpu::Device) -> wgpu::Buffer {
+        device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Screen Space buffer"),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            contents: bytemuck::cast_slice(&[uniform]),
+        })
     }
 
     pub fn get_binding_resource(&self) -> wgpu::BindingResource<'_> {
